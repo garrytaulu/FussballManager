da6ef011f83b0960c35ff2924dd8dfc4dc3f7b6c
bretthop <brett.hoppz@gmail.com>
2013-03-04 18:17:04 +1100

    * Cleaned project

--- server/app/controllers/dashboard_controller.rb
+++
@@ -1,4 +0,0 @@
-class DashboardController < ApplicationController
-  def welcome
-  end
-end
--- server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FussballManager::Application.config.secret_token = '67743e146d90f02428ec63e0c0b4dafaa2fa3402215af2442ac24b810f4a5f87feeb5daa9959e72797c27464ea7ac2deab57fe936b3f1ca887356095610674d7'
