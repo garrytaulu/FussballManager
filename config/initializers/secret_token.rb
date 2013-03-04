f2099c06116d7b22f8ead9c145ded8ae6e687e90
bretthop <brett.hoppz@gmail.com>
2013-03-04 18:23:25 +1100

    * Added easier initial project struct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FussballManager::Application.config.secret_token = 'dd92c209047d348c3cc0dba592c7271bbee33e0a20001d074956683ef3bdf4b7fa902d3d3c2fd306c9c8ef3af70db6b461e75a1faa1a6fac2ed0e753feb0cf85'
