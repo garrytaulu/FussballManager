13383e0ba6e36fef4e8f918e3ec757202fcec04b
Alex Lambrinos <alex.lambrinos@gmail.com>
2013-04-11 23:13:57 +1000

    Hot fix for scores resource to return the score object not a [game, score] array.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,7 +3,7 @@ class ScoresController < ApplicationController
   # GET games/1/scores
   # GET games/1/scores.json
   def index
-    @scores = Score.order('created_at').find_all_by_game(params[:game_id])
+    @scores = Score.find_all_by_game(params[:game_id], :order => 'created_at')
 
     respond_with @scores
   end
@@ -19,68 +19,25 @@ class ScoresController < ApplicationController
   # GET games/1/scores/new
   # GET games/1/scores/new.json
   def new
-    @game = Game.find(params[:game_id])
     @score = Score.new
+    @score.game = Game.find(params[:game_id])
   end
 
   # GET games/1/scores/1/edit
   def edit
     @score = Score.find(params[:id])
-    @game = @score.game
   end
 
   # POST games/1/scores
   # POST games/1/scores.json
   def create
-    @game = Game.find(params[:score][:game])
-    @score = Score.new
-    @score.game = @game
-    @score.player = Player.find(params[:score][:player])
-    @score.own_goal = params[:score][:own_goal]
-
-    # work out which team scored
-    @score.team = 'blue'
-
-    if @score.player.id == @score.game.blueAttacker.id || @score.player.id == @score.game.blueDefender.id
-      if @score.own_goal
-        @score.team = 'red'
-      end
-    else
-        @score.team = 'red'
-        if @score.own_goal
-          @score.team = 'blue'
-        end
-    end
-
-    @score.save
-
-    respond_with [@game, @score]
+    respond_with upsert_score(params)
   end
 
   # PUT games/1/scores/1
   # PUT games/1/scores/1.json
   def update
-    @score = Score.find(params[:id])
-    @score.player = Player.find(params[:score][:player])
-    @score.own_goal = params[:score][:own_goal]
-
-    # work out which team scored
-    @score.team = 'blue'
-
-    if @score.player.id == @score.game.blueAttacker.id || @score.player.id == @score.game.blueDefender.id
-      if @score.own_goal
-        @score.team = 'red'
-      end
-    else
-      @score.team = 'red'
-      if @score.own_goal
-        @score.team = 'blue'
-      end
-    end
-
-    @score.save
-
-    respond_with [@score.game, @score]
+    respond_with upsert_score(params)
   end
 
   # DELETE games/1/scores/1
@@ -90,5 +47,46 @@ class ScoresController < ApplicationController
     @score.destroy
 
     respond_with [@score.game, @score]
+  end
+
+  private
+
+  # Inserts or updated a game score.
+  def upsert_score(params)
+
+    return unless params
+
+    if params[:id]
+      score = Score.find(params[:id])
+    else
+      score = Score.new
+      score.game = Game.find(params[:game_id])
+    end
+
+    score.player = Player.find(params[:score][:player])
+    score.own_goal = params[:score][:own_goal]
+
+    # work out which team scored by first
+    # checking if its a blue player
+    flag = score.player.id == score.game.blueAttacker.id ||
+           score.player.id == score.game.blueDefender.id
+
+    # if it's an own goal then flip flag otherwise do nothing
+    flag = score.own_goal && !flag || flag
+
+    if flag
+      score.team = 'blue'
+    else
+      score.team = 'red'
+    end
+
+    score.save
+
+    # TODO: remove this and do it a different way if possible.
+    if response.request.headers['CONTENT_TYPE'] == "application/x-www-form-urlencoded"
+      [score.game, score]
+    else
+      score
+    end
   end
 end
