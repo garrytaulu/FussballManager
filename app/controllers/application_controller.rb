3fa1b36a8ba3b932671c82e1e526b08b4fb8a07f
Alex Lambrinos <alex.lambrinos@gmail.com>
2013-04-13 19:14:15 +1000

    Updated scores resource json responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,23 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  # all controllers respond to html, json and xml.
-  respond_to :html, :json, :xml
+  # all controllers respond to html and json.
+  respond_to :html, :json
+
+  # handler for 404 exceptions
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  private
+
+  def record_not_found
+
+    respond_to do |format|
+      format.html do
+        render :status => :not_found, :text => 'Resource not found'
+      end
+      format.json do
+        render :status => :not_found, :nothing => true
+      end
+    end
+  end
 end
--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,7 +3,8 @@ class ScoresController < ApplicationController
   # GET games/1/scores
   # GET games/1/scores.json
   def index
-    @scores = Score.find_all_by_game(params[:game_id], :order => 'created_at')
+    game = Game.find(params[:game_id])
+    @scores = Score.find_all_by_game(game.id, :order => 'created_at')
 
     respond_with @scores
   end
@@ -13,31 +14,80 @@ class ScoresController < ApplicationController
   def show
     @score = Score.find(params[:id])
 
-    respond_with @score
+    respond_to do |format|
+      format.html
+      format.json do
+        render :json => @score
+      end
+    end
   end
 
   # GET games/1/scores/new
-  # GET games/1/scores/new.json
   def new
     @score = Score.new
     @score.game = Game.find(params[:game_id])
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render :status => :not_found
+      end
+    end
   end
 
   # GET games/1/scores/1/edit
   def edit
     @score = Score.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render :status => :not_found
+      end
+    end
   end
 
   # POST games/1/scores
   # POST games/1/scores.json
   def create
-    respond_with upsert_score(params)
+    @score = Score.new
+    @score.game = Game.find(params[:game_id])
+    @score.player = Player.find(params[:score][:player])
+    @score.own_goal = params[:score][:own_goal]
+
+    set_team(@score)
+    @score.save
+
+    respond_to do |format|
+      format.html do
+        redirect_to game_score_path([@score.game, @score])
+      end
+      format.json do
+        render :status => :created,
+               :location => game_score_path(@score.game, @score),
+               :json => {:id => @score.id }
+      end
+    end
   end
 
   # PUT games/1/scores/1
   # PUT games/1/scores/1.json
   def update
-    respond_with upsert_score(params)
+    @score = Score.find(params[:id])
+    @score.player = Player.find(params[:score][:player])
+    @score.own_goal = params[:score][:own_goal]
+
+    set_team(@score)
+    @score.save
+
+    respond_to do |format|
+      format.html do
+        redirect_to game_score_path(@score.game, @score)
+      end
+      format.json do
+        render :nothing => true
+      end
+    end
   end
 
   # DELETE games/1/scores/1
@@ -46,47 +96,34 @@ class ScoresController < ApplicationController
     @score = Score.find(params[:id])
     @score.destroy
 
-    respond_with [@score.game, @score]
+    respond_to do |format|
+      format.html do
+        redirect_to game_scores_path
+      end
+      format.json do
+        render :nothing => true
+      end
+    end
   end
 
   private
 
-  # Inserts or updated a game score.
-  def upsert_score(params)
+  def set_team(score)
 
-    return unless params
-
-    if params[:id]
-      score = Score.find(params[:id])
-    else
-      score = Score.new
-      score.game = Game.find(params[:game_id])
-    end
-
-    score.player = Player.find(params[:score][:player])
-    score.own_goal = params[:score][:own_goal]
+    return unless score
 
     # work out which team scored by first
     # checking if its a blue player
     flag = score.player.id == score.game.blueAttacker.id ||
-           score.player.id == score.game.blueDefender.id
+        score.player.id == score.game.blueDefender.id
 
     # if it's an own goal then flip flag otherwise do nothing
-    flag = score.own_goal && !flag || flag
+    flag = score.own_goal && !flag
 
     if flag
       score.team = 'blue'
     else
       score.team = 'red'
-    end
-
-    score.save
-
-    # TODO: remove this and do it a different way if possible.
-    if response.request.headers['CONTENT_TYPE'] == "application/x-www-form-urlencoded"
-      [score.game, score]
-    else
-      score
     end
   end
 end
